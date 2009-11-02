6e72b58e76bf00b1c4c59702357b1a3324fa9815
Akbar Nurlybayev <akbar@oanda.com>
2009-11-01 21:01:19 -0500

    Adding .js files.

---
+++ calc.js
@@ -0,0 +1,11 @@
+var goraHash = new Hash();
+goraHash.set("p1", $('gora_p1'));
+gorahash.set("p2", $('gora_p2'));
+goraHash.set("p3", $('gora_p3'));
+
+var whistHash = new Hash();
+whistHash.set("p1", $('whist_p1'));
+whistHash.set("p2", $('whist_p2'));
+whistHash.set("p3", $('whist_p3'));
+
+var sortedGora = goraHash.values().sort();
